0ace3d077ed1718e6ae28dadce87febd7a5ea562
Pabloz777 <pavel.maliasov@yandex.ru>
2019-03-30 16:13:53 +0700

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,23 @@
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = (x * y + 5 * z + x - 1);
+
+console.log(result);
+
+// Задача 2:
+
+let algebra = 3;
+let geography = 4;
+let physics = 5;
+
+let average = ((algebra + geography + physics) / 3);
+
+console.log(average);
+
+// Задача 3:
+
+let name = (" Паша");
+let message = (`Привет, мир! Меня зовут${name}`);
+console.log(message);
